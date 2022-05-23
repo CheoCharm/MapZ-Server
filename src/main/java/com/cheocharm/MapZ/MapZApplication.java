cdbbcba3e9ef4a16554e28f0527509496744b72a
choigy1001 <easytwo10@naver.com>
2022-05-23 23:17:37 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/cheocharm/MapZ/MapZApplication.java
@@ -0,0 +1,13 @@
+package com.cheocharm.MapZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheocharm/MapZ/MapZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheocharm.MapZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
