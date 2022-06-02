31cb4cfc9235ff4d1e1d8662bb59cb54b99271d4
choigy1001 <easytwo10@naver.com>
2022-06-02 23:04:47 +0900

    feat: 생성, 업데이트 시간 자동화

--- src/main/java/com/cheocharm/MapZ/MapZApplication.java
+++ src/main/java/com/cheocharm/MapZ/MapZApplication.java
@@ -2,7 +2,9 @@ package com.cheocharm.MapZ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MapZApplication {
 
