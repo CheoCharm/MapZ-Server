f03f41fa6e4830be41dbc36c560dfed69248c0e5
choigy1001 <easytwo10@naver.com>
2022-06-06 22:20:37 +0900

    feat: Security Config 임시 설정

---
+++ src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.cheocharm.MapZ.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().anyRequest().permitAll();
+
+    }
+
+}
