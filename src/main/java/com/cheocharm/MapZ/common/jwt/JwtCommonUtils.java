96e7872ae314ea739f5b096f0d30c7fc9cc051a8
choigy1001 <easytwo10@naver.com>
2022-06-09 16:38:33 +0900

    feat: jwt 에서 공통으로 사용하는 util 클래스

---
+++ src/main/java/com/cheocharm/MapZ/common/jwt/JwtCommonUtils.java
@@ -0,0 +1,48 @@
+package com.cheocharm.MapZ.common.jwt;
+
+import com.cheocharm.MapZ.user.domain.UserEntity;
+import com.cheocharm.MapZ.user.domain.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@RequiredArgsConstructor
+@Component
+public class JwtCommonUtils {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    private final String USER_EMAIL = "email";
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    protected void init() {
+        SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public UserEntity findUserByToken(String token) {
+        return userRepository.findByEmail(findEmailByToken(token))
+                .orElseThrow(() -> new RuntimeException("이메일 유저 정보 없음"));
+    }
+
+    public String findEmailByToken(String token) {
+        return (String) Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get(USER_EMAIL);
+    }
+
+    public String getKey() {
+        return SECRET_KEY;
+    }
+}
