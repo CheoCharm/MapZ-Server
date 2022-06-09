5cfdb39d65908341a47a8816bc25938b3a2f0639
choigy1001 <easytwo10@naver.com>
2022-06-09 16:35:15 +0900

    feat: jwt 추가에 따른 권한 설정 및 필터 활성화

--- src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
+++ src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.cheocharm.MapZ.common.config;
 
+import com.cheocharm.MapZ.common.jwt.JwtAuthenticationFilter;
+import com.cheocharm.MapZ.common.jwt.JwtAuthenticationUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    private final JwtAuthenticationUtils jwtAuthenticationUtils;
+    private final AuthenticationEntryPointCustom authenticationEntryPointCustom;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -25,8 +31,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().anyRequest().permitAll();
+                .authorizeRequests()
+                    .anyRequest().hasAuthority("ROLE_USER")
+                    .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPointCustom);
 
+         http.addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationUtils), UsernamePasswordAuthenticationFilter.class);
     }
 
 }
