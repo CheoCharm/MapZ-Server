d23bc677f88d1e9a066a154c7b6f82620f24d469
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-17 00:04:53 +0900

    fix: INTERNAL_SERVER_ERROR 삭제

--- src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
@@ -9,8 +9,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @Getter
 public enum ExceptionDetails {
     // 공통 에러
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "0001", "서버 오류입니다. 서버측에 문의주세요.", Exception.class),
-    NOT_FOUND_API(HttpStatus.NOT_FOUND, "0002", "존재하지 않는 API입니다. 요청 경로를 확인해주세요.", NoHandlerFoundException.class);
+    NOT_FOUND_API(HttpStatus.NOT_FOUND, "0001", "존재하지 않는 API입니다. 요청 경로를 확인해주세요.", NoHandlerFoundException.class);
 
     //사용자 관련 에러
 
