584521a5143185056f34d9a86e3df3a59e834955
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-22 21:38:05 +0900

    fix: ExceptionDetails에서 오류 type 제거

--- src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
@@ -3,13 +3,12 @@ package com.cheocharm.MapZ.common.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @AllArgsConstructor
 @Getter
 public enum ExceptionDetails {
     // 공통 에러
-    NOT_FOUND_API(HttpStatus.NOT_FOUND, "0001", "존재하지 않는 API입니다. 요청 경로를 확인해주세요.", NoHandlerFoundException.class);
+    NOT_FOUND_API(HttpStatus.NOT_FOUND, "0001", "존재하지 않는 API입니다. 요청 경로를 확인해주세요.");
 
     //사용자 관련 에러
 
@@ -18,5 +17,4 @@ public enum ExceptionDetails {
     private HttpStatus statusCode;
     private String customCode;
     private String message;
-    private final Class<? extends Exception> type;
 }
