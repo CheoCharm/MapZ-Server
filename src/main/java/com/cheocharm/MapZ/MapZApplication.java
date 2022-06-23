b0bfc3011b8aaf828907066b81641fb8382833a9
choigy1001 <easytwo10@naver.com>
2022-06-24 00:06:59 +0900

    feat: Configuration Properties scan 패키지 추가

--- src/main/java/com/cheocharm/MapZ/MapZApplication.java
+++ src/main/java/com/cheocharm/MapZ/MapZApplication.java
@@ -2,8 +2,10 @@ package com.cheocharm.MapZ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ConfigurationPropertiesScan("com.cheocharm.MapZ.common.oauth")
 @EnableJpaAuditing
 @SpringBootApplication
 public class MapZApplication {
