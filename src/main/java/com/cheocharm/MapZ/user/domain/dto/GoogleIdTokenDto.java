a43b0fc2194b3710dc7ba09627199dc23621116a
choigy1001 <easytwo10@naver.com>
2022-06-24 00:12:30 +0900

    feat: 구글 idToken Dto 클래스 추가

---
+++ src/main/java/com/cheocharm/MapZ/user/domain/dto/GoogleIdTokenDto.java
@@ -0,0 +1,20 @@
+package com.cheocharm.MapZ.user.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class GoogleIdTokenDto {
+    private String iss;
+    private String sub;
+    private String azp;
+    private String aud;
+    private String iat;
+    private String exp;
+    private String email;
+    private String email_verified;
+    private String name;
+    private String picture;
+    private String given_name;
+    private String family_name;
+    private String locale;
+}
