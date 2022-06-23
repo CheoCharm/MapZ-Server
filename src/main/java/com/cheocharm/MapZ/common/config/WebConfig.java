5ceb6296fdc3d5071a6ec5c26ec792715882bea6
choigy1001 <easytwo10@naver.com>
2022-06-24 00:07:49 +0900

    feat: restTemplate 추가 및 excludePathPattern 추가

--- src/main/java/com/cheocharm/MapZ/common/config/WebConfig.java
+++ src/main/java/com/cheocharm/MapZ/common/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.cheocharm.MapZ.common.config;
 
 import com.cheocharm.MapZ.common.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,6 +27,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/api/**")
-                .excludePathPatterns();
+                .excludePathPatterns("/api/users", "/api/users/login");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
