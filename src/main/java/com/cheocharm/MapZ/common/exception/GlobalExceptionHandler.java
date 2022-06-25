5e3b210a99aee1c4f6e998f8f31baf4eb7df9f6a
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-25 20:24:57 +0900

    fix: Common Response에 custom code 추가

--- src/main/java/com/cheocharm/MapZ/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/GlobalExceptionHandler.java
@@ -4,17 +4,25 @@ import com.cheocharm.MapZ.common.CommonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     protected CommonResponse handleCustomException(CustomException ex) {
-        return CommonResponse.fail(ex.getStatusCode(), ex.getMessage());
+        return CommonResponse.fail(ex.getStatusCode(), ex.getCustomCode(), ex.getMessage());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected CommonResponse handleNoHandlerFoundException(NoHandlerFoundException exception) {
+        ExceptionDetails exceptionDetails = ExceptionDetails.NOT_FOUND_API;
+        return CommonResponse.fail(exceptionDetails.getStatusCode(), exceptionDetails.getCustomCode(), exceptionDetails.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     protected CommonResponse handleException(Exception ex) {
-        return CommonResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        ExceptionDetails exceptionDetails = ExceptionDetails.INTERNAL_SERVER_ERROR;
+        return CommonResponse.fail(exceptionDetails.getStatusCode(), exceptionDetails.getCustomCode(), exceptionDetails.getMessage());
     }
 }
