28f3b08caad3ea6795c90fef09deeb0c8243e429
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-27 23:14:00 +0900

    feat: AspectJ 설정

--- src/main/java/com/cheocharm/MapZ/MapZApplication.java
+++ src/main/java/com/cheocharm/MapZ/MapZApplication.java
@@ -3,9 +3,11 @@ package com.cheocharm.MapZ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan("com.cheocharm.MapZ.common.oauth")
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
 public class MapZApplication {
