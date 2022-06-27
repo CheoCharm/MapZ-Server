8d7bc3b3cb32c5c63d26f1ee55c7f3b6c19d7036
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-27 23:13:44 +0900

    feat: Response Log 구현

---
+++ src/main/java/com/cheocharm/MapZ/common/log/response/ResponseLogSchema.java
@@ -0,0 +1,42 @@
+package com.cheocharm.MapZ.common.log.response;
+
+import com.cheocharm.MapZ.common.CommonResponse;
+import com.cheocharm.MapZ.common.exception.ExceptionDetails;
+import com.cheocharm.MapZ.common.exception.InternalServerException;
+import com.cheocharm.MapZ.common.util.ObjectMapperUtils;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Getter
+@Builder
+public class ResponseLogSchema {
+    private String api;
+    private String httpMethod;
+    private int statusCode;
+    private String message;
+
+    public static ResponseLogSchema createLogSchema(HttpServletRequest request, CommonResponse response) {
+        return ResponseLogSchema.builder()
+                .api(request.getRequestURI())
+                .httpMethod(request.getMethod())
+                .statusCode(response.getStatusCode())
+                .message(response.getMessage())
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        String json = "";
+        try {
+            json = ObjectMapperUtils.getObjectMapper().writeValueAsString(this);
+        } catch (IOException e) {
+            throw new InternalServerException(ExceptionDetails.FAIL_CONVERT_TO_JSON);
+        }
+
+        return json;
+    }
+
+}
