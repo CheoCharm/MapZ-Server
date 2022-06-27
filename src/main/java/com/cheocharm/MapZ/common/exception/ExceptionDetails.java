730ecbb97e0ef02f20e611321514832f69485a30
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-06-27 23:16:09 +0900

    feat: Exception 추가

--- src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionDetails {
     // 공통 에러
     NOT_FOUND_API(HttpStatus.NOT_FOUND, "0001", "존재하지 않는 API입니다. 요청 경로를 확인해주세요."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "0002", "서버 에러입니다. 서버측에 문의주세요."),
+    FAIL_CONVERT_TO_JSON(HttpStatus.INTERNAL_SERVER_ERROR, "0003", "JSON으로 변환하는 도중 오류가 발생하였습니다."),
 
     //사용자 관련 에러
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "2001", "유효하지 않은 JWT 입니다. 토큰을 다시 확인해주세요");
---
+++ src/main/java/com/cheocharm/MapZ/common/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.cheocharm.MapZ.common.exception;
+
+public class InternalServerException extends CustomException {
+    public InternalServerException(ExceptionDetails exceptionDetails) {
+        super(exceptionDetails);
+    }
+}
