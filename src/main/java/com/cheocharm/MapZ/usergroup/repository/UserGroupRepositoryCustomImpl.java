762dbd53ccda98b76033a68810b268e5ed0e9010
choigy1001 <easytwo10@naver.com>
2022-07-05 13:26:11 +0900

    feat: 그룹 조회 기능을 위한 Custom Repository, Impl 추가

---
+++ src/main/java/com/cheocharm/MapZ/usergroup/repository/UserGroupRepositoryCustomImpl.java
@@ -0,0 +1,55 @@
+package com.cheocharm.MapZ.usergroup.repository;
+
+import com.cheocharm.MapZ.group.domain.GroupEntity;
+import com.cheocharm.MapZ.user.domain.UserEntity;
+import com.cheocharm.MapZ.usergroup.UserGroupEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.cheocharm.MapZ.group.domain.QGroupEntity.*;
+import static com.cheocharm.MapZ.user.domain.QUserEntity.*;
+import static com.cheocharm.MapZ.usergroup.QUserGroupEntity.*;
+
+@RequiredArgsConstructor
+@Component
+public class UserGroupRepositoryCustomImpl implements UserGroupRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<UserGroupEntity> fetchJoinByUserEntity(UserEntity userEntity) {
+        return fetchJoinQuery()
+                .where(userEq(userEntity))
+                .fetch();
+    }
+
+    @Override
+    public List<String> findUserImage(GroupEntity groupEntity) {
+        return queryFactory
+                .select(userGroupEntity.userEntity.userImageUrl)
+                .from(userGroupEntity)
+                .where(groupEq(groupEntity))
+                .fetch();
+    }
+
+    private JPAQuery<UserGroupEntity> fetchJoinQuery() {
+        return queryFactory
+                .selectFrom(userGroupEntity)
+                .innerJoin(userGroupEntity.groupEntity, groupEntity)
+                .innerJoin(userGroupEntity.userEntity, userEntity)
+                .fetchJoin();
+    }
+
+    private BooleanExpression userEq(UserEntity userCond) {
+        return userGroupEntity.userEntity.eq(userCond);
+    }
+
+    private BooleanExpression groupEq(GroupEntity groupCond) {
+        return userGroupEntity.groupEntity.eq(groupCond);
+    }
+}
