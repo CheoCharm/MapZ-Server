2e99a7c990c41d5e2ca33ef4c8693bb70055fc0e
choigy1001 <easytwo10@naver.com>
2022-07-05 13:23:46 +0900

    feat: 그룹 기능 관련 Controller 추가

--- src/main/java/com/cheocharm/MapZ/group/domain/GroupController.java
+++ src/main/java/com/cheocharm/MapZ/group/domain/GroupController.java
@@ -1,9 +1,17 @@
 package com.cheocharm.MapZ.group.domain;
 
+import com.cheocharm.MapZ.common.CommonResponse;
+import com.cheocharm.MapZ.group.domain.dto.CreateGroupDto;
+import com.cheocharm.MapZ.group.domain.dto.GroupListDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Tag(name = "GroupController")
 @RequiredArgsConstructor
@@ -11,4 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GroupController {
 
+    private final GroupService groupService;
+
+    @Operation(description = "그룹 생성")
+    @Parameter(name = "accessToken", in = ParameterIn.HEADER, required = true)
+    @PostMapping
+    public CommonResponse<?> createGroup(@RequestBody @Valid CreateGroupDto createGroupDto) {
+        groupService.createGroup(createGroupDto);
+        return CommonResponse.success();
+    }
+
+    @Operation(description = "그룹 선택을 위한 그룹 조회")
+    @Parameter(name = "accessToken", in = ParameterIn.HEADER, required = true)
+    @GetMapping
+    public CommonResponse<List<GroupListDto>> getGroup() {
+        return CommonResponse.success(groupService.getGroup());
+    }
 }
