0c00a4cee8a8f142571fb4303dc0575680550509
choigy1001 <easytwo10@naver.com>
2022-07-05 13:23:15 +0900

    feat: 그룹 저장을 위한 Repository 생성

---
+++ src/main/java/com/cheocharm/MapZ/group/domain/repository/GroupRepository.java
@@ -0,0 +1,7 @@
+package com.cheocharm.MapZ.group.domain.repository;
+
+import com.cheocharm.MapZ.group.domain.GroupEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
+}
