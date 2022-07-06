0afe9921deca5703e7c02ab55feea6b8388a6a15
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-07-07 00:19:47 +0900

    feat: User 관련 exception 추가

--- src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/ExceptionDetails.java
@@ -17,8 +17,12 @@ public enum ExceptionDetails {
     FAIL_DELETE_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "1002", "로컬에 저장된 이미지를 삭제하는데 실패하였습니다."),
 
     // 사용자 관련 에러
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "2001", "유효하지 않은 JWT 입니다. 토큰을 다시 확인해주세요");
-
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "2001", "유효하지 않은 JWT 입니다. 토큰을 다시 확인해주세요"),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "2002", "중복된 이메일입니다."),
+    DUPLICATED_USERNAME(HttpStatus.BAD_REQUEST, "2003", "중복된 닉네임입니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "2004", "가입된 사용자가 아닙니다."),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "2005", "비밀번호가 일치하지 않습니다."),
+    INVALID_USER_INFO(HttpStatus.BAD_REQUEST, "2006", "유효성 검사 통과에 실패하였습니다");
 
     // 비즈니스 로직 에러
 
