58b426488c85dca8d4b6878d90bf248586e7c08d
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-07-07 00:29:04 +0900

    feat: 패스워드 암호화 추가

--- src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
+++ src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.cheocharm.MapZ.common.config;
 import com.cheocharm.MapZ.common.jwt.JwtAuthenticationFilter;
 import com.cheocharm.MapZ.common.jwt.JwtAuthenticationUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
@@ -48,6 +51,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPointCustom);
 
          http.addFilterBefore(new JwtAuthenticationFilter(jwtAuthenticationUtils), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
