7c017e80e25dd99cc68554529b6b6f9e2692e672
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-07-07 00:27:53 +0900

    feat: 닉네임으로 유저 찾기

--- src/main/java/com/cheocharm/MapZ/user/domain/repository/UserRepository.java
+++ src/main/java/com/cheocharm/MapZ/user/domain/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
 }
