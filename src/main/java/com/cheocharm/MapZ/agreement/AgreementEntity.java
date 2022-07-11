a2bd971d14c05889ba4826787c1f519e267d4796
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-07-11 23:50:29 +0900

    feat: AgreementEntity 생성자 추가

--- src/main/java/com/cheocharm/MapZ/agreement/AgreementEntity.java
+++ src/main/java/com/cheocharm/MapZ/agreement/AgreementEntity.java
@@ -3,6 +3,7 @@ package com.cheocharm.MapZ.agreement;
 import com.cheocharm.MapZ.common.domain.BaseEntity;
 import com.cheocharm.MapZ.user.domain.UserEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +22,12 @@ public class AgreementEntity extends BaseEntity {
     private Boolean userInfoAgreement;
 
     private Boolean pushAgreement;
+
+    @Builder
+    public AgreementEntity(UserEntity userEntity, Boolean pushAgreement) {
+        this.userEntity = userEntity;
+        this.userInfoAgreement = Boolean.TRUE;
+        this.pushAgreement = pushAgreement;
+    }
+
 }
