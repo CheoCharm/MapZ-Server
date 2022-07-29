5373825488aead0aaacdac1477288b02758684ab
choigy1001 <easytwo10@naver.com>
2022-07-30 01:16:12 +0900

    feat: 그룹검색 Querydsl 메서드 작성

--- src/main/java/com/cheocharm/MapZ/usergroup/repository/UserGroupRepositoryCustom.java
+++ src/main/java/com/cheocharm/MapZ/usergroup/repository/UserGroupRepositoryCustom.java
@@ -3,11 +3,15 @@ package com.cheocharm.MapZ.usergroup.repository;
 import com.cheocharm.MapZ.group.domain.GroupEntity;
 import com.cheocharm.MapZ.user.domain.UserEntity;
 import com.cheocharm.MapZ.usergroup.UserGroupEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface UserGroupRepositoryCustom {
     List<UserGroupEntity> fetchJoinByUserEntity(UserEntity userEntity);
+
+    Slice<UserGroupEntity> fetchByUserEntityAndSearchNameAndOrderByUserName(UserEntity userEntity, String searchName, Pageable pageable);
 
     List<String> findUserImage(GroupEntity groupEntity);
 }
