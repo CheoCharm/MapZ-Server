37dfc7f73c4de866c180b90018815a56b3727bba
choigy1001 <easytwo10@naver.com>
2022-07-30 20:28:22 +0900

    feat: valid 추가

--- src/main/java/com/cheocharm/MapZ/group/domain/dto/SearchGroupDto.java
+++ src/main/java/com/cheocharm/MapZ/group/domain/dto/SearchGroupDto.java
@@ -2,8 +2,14 @@ package com.cheocharm.MapZ.group.domain.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class SearchGroupDto {
+
+    @NotNull
     Integer page;
+
+    @NotNull
     String searchName;
 }
