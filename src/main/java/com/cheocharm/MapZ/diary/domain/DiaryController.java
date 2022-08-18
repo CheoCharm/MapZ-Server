8bbedec510af06f2b160677ede3c5058546224fb
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-08-19 00:33:17 +0900

    feat: 다이어리 좋아요 controller

--- src/main/java/com/cheocharm/MapZ/diary/domain/DiaryController.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/DiaryController.java
@@ -1,9 +1,14 @@
 package com.cheocharm.MapZ.diary.domain;
 
+import com.cheocharm.MapZ.common.CommonResponse;
+import com.cheocharm.MapZ.diary.domain.dto.LikeDiaryDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Tag(name = "DiaryController")
 @RequiredArgsConstructor
@@ -11,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DiaryController {
 
+    private final DiaryService diaryService;
+
+    @Operation(description = "일기 좋아요")
+    @PutMapping("/like")
+    public CommonResponse<?> likeDiary(@Parameter @RequestBody @Valid LikeDiaryDto likeDiaryDto) {
+        diaryService.likeDiary(likeDiaryDto);
+        return CommonResponse.success();
+    }
 }
