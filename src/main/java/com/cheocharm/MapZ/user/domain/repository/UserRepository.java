96acbf0c16ae6582cb0670989820453d8aa2e12f
choigy1001 <easytwo10@naver.com>
2022-08-18 23:36:40 +0900

    feat: querydsl 구현할 인터페이스 추가

--- src/main/java/com/cheocharm/MapZ/user/domain/repository/UserRepository.java
+++ src/main/java/com/cheocharm/MapZ/user/domain/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long>, UserRepositoryCustom {
     Optional<UserEntity> findByEmailAndUserProvider(String email, UserProvider userProvider);
 
     Optional<UserEntity> findByUsername(String username);
