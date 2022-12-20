58f98533d11df02d34e105ed8d7616a16a9e5375
choigy1001 <easytwo10@naver.com>
2022-12-20 16:30:04 +0900

    feat: 그룹별 그룹장 이미지 획득을 위한 VO 생성

---
+++ src/main/java/com/cheocharm/MapZ/usergroup/repository/vo/ChiefUserImageVO.java
@@ -0,0 +1,16 @@
+package com.cheocharm.MapZ.usergroup.repository.vo;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+@Getter
+public class ChiefUserImageVO {
+    private String chiefUserImage;
+    private Long id;
+
+    @QueryProjection
+    public ChiefUserImageVO(String chiefUserImage, Long id) {
+        this.chiefUserImage = chiefUserImage;
+        this.id = id;
+    }
+}
