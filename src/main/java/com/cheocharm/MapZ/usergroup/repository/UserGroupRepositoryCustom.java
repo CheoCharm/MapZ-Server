cdd6b1d73db7513ef767f9252b6161605421856c
choigy1001 <easytwo10@naver.com>
2022-12-20 16:32:22 +0900

    feat: 그룹별 그룹장, 인원수 획득을 위한 QueryDSL 메서드 추가

--- src/main/java/com/cheocharm/MapZ/usergroup/repository/UserGroupRepositoryCustom.java
+++ src/main/java/com/cheocharm/MapZ/usergroup/repository/UserGroupRepositoryCustom.java
@@ -3,13 +3,18 @@ package com.cheocharm.MapZ.usergroup.repository;
 import com.cheocharm.MapZ.group.domain.GroupEntity;
 import com.cheocharm.MapZ.user.domain.UserEntity;
 import com.cheocharm.MapZ.usergroup.UserGroupEntity;
+import com.cheocharm.MapZ.usergroup.repository.vo.ChiefUserImageVO;
+import com.cheocharm.MapZ.usergroup.repository.vo.CountUserGroupVO;
 
 import java.util.List;
 
 public interface UserGroupRepositoryCustom {
     List<UserGroupEntity> fetchJoinByUserEntity(UserEntity userEntity);
 
-    List<String> findUserImage(GroupEntity groupEntity);
+    List<GroupEntity> getGroupEntityList(UserEntity userEntity);
+    List<ChiefUserImageVO> findChiefUserImage(List<GroupEntity> groupEntityList);
 
     List<UserGroupEntity> findBySearchNameAndGroupEntity(String searchName, GroupEntity groupEntity);
+
+    List<CountUserGroupVO> countByGroupEntity(List<GroupEntity> groupEntityList);
 }
