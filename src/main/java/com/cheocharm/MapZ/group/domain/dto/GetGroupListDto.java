c41de6bc95342bd4e2c454e4132e7b43d5cd8702
choigy1001 <easytwo10@naver.com>
2022-12-20 16:31:34 +0900

    feat: 그룹별 그룹장 이미지만 Response 주기로 변경

--- src/main/java/com/cheocharm/MapZ/group/domain/dto/GetGroupListDto.java
+++ src/main/java/com/cheocharm/MapZ/group/domain/dto/GetGroupListDto.java
@@ -3,15 +3,13 @@ package com.cheocharm.MapZ.group.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @Builder
 public class GetGroupListDto {
 
     private String groupName;
     private String groupImageUrl;
-    private int count;
-    private List<String> userImageUrlList;
+    private Long count;
+    private String chiefUserImage;
 
 }
