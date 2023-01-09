c2687aaa39992ec3ff31185868714d7540a6249a
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-01-09 17:55:44 +0900

    feat: comment controller, service, repository

---
+++ src/main/java/com/cheocharm/MapZ/comment/domain/CommentService.java
@@ -0,0 +1,38 @@
+package com.cheocharm.MapZ.comment.domain;
+
+import com.cheocharm.MapZ.comment.domain.dto.CreateCommentDto;
+import com.cheocharm.MapZ.comment.domain.repository.CommentRepository;
+import com.cheocharm.MapZ.common.exception.diary.NotFoundDiaryException;
+import com.cheocharm.MapZ.common.interceptor.UserThreadLocal;
+import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.cheocharm.MapZ.diary.domain.respository.DiaryRepository;
+import com.cheocharm.MapZ.user.domain.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final DiaryRepository diaryRepository;
+
+    @Transactional
+    public void createComment(CreateCommentDto createCommentDto) {
+        final UserEntity userEntity = UserThreadLocal.get();
+        Optional<DiaryEntity> diaryEntity = diaryRepository.findById(createCommentDto.getDiaryId());
+        diaryEntity.orElseThrow(() -> new NotFoundDiaryException());
+        commentRepository.save(
+                CommentEntity.builder()
+                        .content(createCommentDto.getContent())
+                        .parentId(createCommentDto.getParentId())
+                        .userEntity(userEntity)
+                        .diaryEntity(diaryEntity.get())
+                        .build()
+        );
+    }
+}
