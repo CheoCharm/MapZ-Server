0f7027cb3e61b178b9f54e806dae282d41ed732b
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-01-13 15:58:56 +0900

    fix: import Optional 삭제

--- src/main/java/com/cheocharm/MapZ/comment/domain/CommentService.java
+++ src/main/java/com/cheocharm/MapZ/comment/domain/CommentService.java
@@ -11,8 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
