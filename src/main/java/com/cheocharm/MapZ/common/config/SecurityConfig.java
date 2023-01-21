5e5fa9cb744ea529df2a4eea8b1c94950f1184f7
choigy1001 <easytwo10@naver.com>
2023-01-21 21:06:04 +0900

    feat: SecurityConfig API 별로 인증하도록 수정

--- src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
+++ src/main/java/com/cheocharm/MapZ/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.cheocharm.MapZ.common.jwt.JwtAuthenticationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -47,8 +48,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/swagger-ui/**").permitAll()
                 .and()
                 .authorizeRequests()
-                //로그인 예외
-                    .antMatchers("/board/**").hasAuthority("ROLE_USER")
+                    //users
+                    .antMatchers(HttpMethod.POST, "/api/users").permitAll()
+                    .antMatchers(HttpMethod.POST,"/api/users/login").permitAll()
+                    .antMatchers(HttpMethod.GET,"/api/users/valid/email/**").permitAll()
+                    .antMatchers(HttpMethod.POST,"/api/users/signup").permitAll()
+                    .antMatchers(HttpMethod.POST,"/api/users/signin").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/users/password/**").permitAll()
+                    .antMatchers(HttpMethod.PATCH,"/api/users/password/").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/users/user").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/users/mypage").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/users/refresh").authenticated()
+
+                    //group
+                    .antMatchers(HttpMethod.POST, "/api/group").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/group").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/api/group/status").authenticated()
+                    .antMatchers(HttpMethod.POST, "/api/group/join").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/api/group/join").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/api/group/exit").authenticated()
+                    .antMatchers(HttpMethod.POST, "/api/group/invite").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/group/mygroup").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/group/member").authenticated()
+
+                    //diary
+                    .antMatchers(HttpMethod.PUT, "/api/diary/like").authenticated()
+                    .antMatchers(HttpMethod.POST, "/api/diary").authenticated()
+                    .antMatchers(HttpMethod.GET, "/api/diary").authenticated()
+
+                    //commnet
+                    .antMatchers(HttpMethod.POST, "/api/comment").authenticated()
+
+                    .anyRequest().denyAll()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPointCustom);
 
