16bc0d8791a7caec05cfb82ddd0d72ba1eec359c
choigy1001 <easytwo10@naver.com>
2023-01-21 21:03:33 +0900

    feat: 그룹에서 일기 조회하는 QueryDSL 메서드

--- src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustomImpl.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustomImpl.java
@@ -1,5 +1,34 @@
 package com.cheocharm.MapZ.diary.domain.respository;
 
+import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.cheocharm.MapZ.diary.domain.QDiaryEntity.*;
+
+@RequiredArgsConstructor
 public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
 
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<DiaryEntity> findByUserIdAndGroupId(Long userId, Long groupId) {
+        return queryFactory
+                .selectFrom(diaryEntity)
+                .where(userIdEq(userId)
+                        .and(groupIdEq(groupId))
+                )
+                .fetch();
+    }
+
+    private BooleanExpression userIdEq(Long userId) {
+        return diaryEntity.userEntity.id.eq(userId);
+    }
+
+    private BooleanExpression groupIdEq(Long groupId) {
+        return diaryEntity.groupEntity.id.eq(groupId);
+    }
 }
