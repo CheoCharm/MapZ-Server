d816223b65bec1610c56a82d18675af2bce559e0
choigy1001 <easytwo10@naver.com>
2023-01-21 21:04:53 +0900

    feat: 일기 작성, 조회 코드 작성

--- src/main/java/com/cheocharm/MapZ/diary/domain/DiaryService.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/DiaryService.java
@@ -2,14 +2,25 @@ package com.cheocharm.MapZ.diary.domain;
 
 import com.cheocharm.MapZ.common.exception.diary.AlreadyLikedDiaryException;
 import com.cheocharm.MapZ.common.exception.diary.NotFoundDiaryException;
+import com.cheocharm.MapZ.common.exception.group.NotFoundGroupException;
 import com.cheocharm.MapZ.common.interceptor.UserThreadLocal;
+import com.cheocharm.MapZ.diary.domain.dto.GetDiaryListDto;
 import com.cheocharm.MapZ.diary.domain.dto.LikeDiaryDto;
+import com.cheocharm.MapZ.diary.domain.dto.WriteDiaryDto;
 import com.cheocharm.MapZ.diary.domain.respository.DiaryLikeRepository;
 import com.cheocharm.MapZ.diary.domain.respository.DiaryRepository;
+import com.cheocharm.MapZ.group.domain.GroupEntity;
+import com.cheocharm.MapZ.group.domain.repository.GroupRepository;
 import com.cheocharm.MapZ.user.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -18,6 +29,7 @@ public class DiaryService {
 
     private final DiaryRepository diaryRepository;
     private final DiaryLikeRepository diaryLikeRepository;
+    private final GroupRepository groupRepository;
 
     @Transactional
     public void likeDiary(LikeDiaryDto likeDiaryDto) {
@@ -36,5 +48,49 @@ public class DiaryService {
                 .build();
 
         diaryLikeRepository.save(diaryLikeEntity);
+    }
+
+    @Transactional
+    public void writeDiary(WriteDiaryDto writeDiaryDto) {
+        UserEntity userEntity = UserThreadLocal.get();
+
+        GroupEntity groupEntity = groupRepository.findById(writeDiaryDto.getGroupId())
+                .orElseThrow(NotFoundGroupException::new);
+
+        String pointWKT = String.format("POINT(%s %s)", writeDiaryDto.getLatitude(), writeDiaryDto.getLongitude());
+
+        try {
+            Point point = (Point) new WKTReader().read(pointWKT);
+            diaryRepository.save(
+                    DiaryEntity.builder()
+                            .userEntity(userEntity)
+                            .groupEntity(groupEntity)
+                            .title(writeDiaryDto.getTitle())
+                            .content(writeDiaryDto.getContent())
+                            .point(point)
+                            .build()
+            );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public GetDiaryListDto getDiary(Long groupId) {
+        UserEntity userEntity = UserThreadLocal.get();
+        List<DiaryEntity> diaryEntities = diaryRepository.findByUserIdAndGroupId(userEntity.getId(), groupId);
+
+
+        List<GetDiaryListDto.DiaryList> list = diaryEntities.stream()
+                .map(diaryEntity -> GetDiaryListDto.DiaryList.builder()
+                        .x(diaryEntity.getPoint().getX())
+                        .y(diaryEntity.getPoint().getY())
+                        .diaryId(diaryEntity.getId())
+                        .title(diaryEntity.getTitle())
+                        .content(diaryEntity.getContent())
+                        .build()
+                )
+                .collect(Collectors.toList());
+
+        return new GetDiaryListDto(true, list);
     }
 }
