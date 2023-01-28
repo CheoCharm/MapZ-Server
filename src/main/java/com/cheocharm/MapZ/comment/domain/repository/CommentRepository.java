4d6699e176aaa41355a497f02e451f716c68f7a4
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-01-28 11:54:54 +0900

    fix: import문, 메서드 삭제

--- src/main/java/com/cheocharm/MapZ/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/cheocharm/MapZ/comment/domain/repository/CommentRepository.java
@@ -6,11 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.transaction.Transactional;
-
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-    void deleteById(Long id);
-
     @Modifying
     @Query("delete from CommentEntity c where c.parentId in :id")
     void deleteAllByIdInQuery(@Param("id") Long id);
