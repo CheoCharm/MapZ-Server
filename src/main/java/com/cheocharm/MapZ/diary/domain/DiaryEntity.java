dcb35a2db66641b1d27014a17046b3a26cf98581
choigy1001 <easytwo10@naver.com>
2023-01-30 12:52:23 +0900

    feat: DiaryEntity에 댓글, 좋아요 Entity Cascade 적용

--- src/main/java/com/cheocharm/MapZ/diary/domain/DiaryEntity.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/DiaryEntity.java
@@ -1,6 +1,7 @@
 package com.cheocharm.MapZ.diary.domain;
 
 
+import com.cheocharm.MapZ.comment.domain.CommentEntity;
 import com.cheocharm.MapZ.common.domain.BaseEntity;
 import com.cheocharm.MapZ.group.domain.GroupEntity;
 import com.cheocharm.MapZ.user.domain.UserEntity;
@@ -8,14 +9,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Where;
 import org.locationtech.jts.geom.Point;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "Diary")
-@Where(clause = "deleted=0")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class DiaryEntity extends BaseEntity {
@@ -33,6 +34,12 @@ public class DiaryEntity extends BaseEntity {
     @JoinColumn(name = "group_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private GroupEntity groupEntity;
+
+    @OneToMany(mappedBy = "diaryEntity", cascade = CascadeType.REMOVE)
+    private List<CommentEntity> commentEntityList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "diaryEntity", cascade = CascadeType.REMOVE)
+    private List<DiaryLikeEntity> diaryLikeEntityList = new ArrayList<>();
 
     @Builder
     public DiaryEntity(String title, String content, Point point, UserEntity userEntity, GroupEntity groupEntity) {
