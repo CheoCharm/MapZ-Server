2b218eb18e10bb86de96a5304e6da6bddcbe9ca6
choigy1001 <easytwo10@naver.com>
2023-02-01 14:02:30 +0900

    feat: 내가 쓴 일기 조회 QueryDSL 메서드

--- src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustom.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustom.java
@@ -1,9 +1,14 @@
 package com.cheocharm.MapZ.diary.domain.respository;
 
 import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.cheocharm.MapZ.diary.domain.respository.vo.MyDiaryVO;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 public interface DiaryRepositoryCustom {
     List<DiaryEntity> findByUserIdAndGroupId(Long userId, Long groupId);
+
+    Slice<MyDiaryVO> findByUserId(Long userId, Long cursorId, Pageable pageable);
 }
