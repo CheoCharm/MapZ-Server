9ed5dfb6a8c878b83b25d56e8050def3fd0cb264
choigy1001 <easytwo10@naver.com>
2023-02-22 14:26:50 +0900

    feat: Throwable 필드 추가

--- src/main/java/com/cheocharm/MapZ/common/exception/jwt/InvalidJwtException.java
+++ src/main/java/com/cheocharm/MapZ/common/exception/jwt/InvalidJwtException.java
@@ -4,6 +4,10 @@ import com.cheocharm.MapZ.common.exception.CustomException;
 import com.cheocharm.MapZ.common.exception.ExceptionDetails;
 
 public class InvalidJwtException extends CustomException {
+    public InvalidJwtException(Throwable cause) {
+        super(ExceptionDetails.INVALID_TOKEN, cause);
+    }
+
     public InvalidJwtException() {
         super(ExceptionDetails.INVALID_TOKEN);
     }
