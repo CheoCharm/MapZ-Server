1085619cb0eb1d414bfd9e64525f3a8fd599b07a
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-02-28 18:10:42 +0900

    feat: 댓글 조회 VO, DTO

---
+++ src/main/java/com/cheocharm/MapZ/comment/presentation/dto/response/GetCommentResponse.java
@@ -0,0 +1,30 @@
+package com.cheocharm.MapZ.comment.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetCommentResponse {
+
+    private boolean hasNext;
+    private List<Comment> commentList;
+
+    @Getter
+    @Builder
+    public static class Comment {
+        private String imageUrl;
+        private String username;
+        private String createdAt;
+        private String content;
+        private Long commentId;
+        private Long parentId;
+        @JsonProperty("isWriter")
+        private boolean writer;
+        private boolean canDelete;
+    }
+}
