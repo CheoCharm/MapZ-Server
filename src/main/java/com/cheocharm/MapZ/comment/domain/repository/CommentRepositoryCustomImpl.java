6d63631aee05fba201c5b1cb3c47368aa365e6c4
kiiiyeon <rusl0325@sookmyung.ac.kr>
2023-02-28 18:20:33 +0900

    feat: 댓글 조회 서비스

--- src/main/java/com/cheocharm/MapZ/comment/domain/repository/CommentRepositoryCustomImpl.java
+++ src/main/java/com/cheocharm/MapZ/comment/domain/repository/CommentRepositoryCustomImpl.java
@@ -1,12 +1,18 @@
 package com.cheocharm.MapZ.comment.domain.repository;
 
+import com.cheocharm.MapZ.comment.domain.repository.vo.CommentVO;
+import com.cheocharm.MapZ.comment.domain.repository.vo.QCommentVO;
 import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
 import static com.cheocharm.MapZ.comment.domain.QCommentEntity.*;
+import static com.cheocharm.MapZ.common.util.QuerydslSupport.fetchSliceByCursor;
 
 @RequiredArgsConstructor
 public class CommentRepositoryCustomImpl implements CommentRepositoryCustom{
@@ -20,4 +26,26 @@ public class CommentRepositoryCustomImpl implements CommentRepositoryCustom{
                 .where(commentEntity.diaryEntity.in(diaryEntityList))
                 .execute();
     }
+
+    @Override
+    public Slice<CommentVO> findByDiaryId(Long diaryId, Long cursorId, Pageable pageable) {
+        JPAQuery<CommentVO> query = queryFactory
+                .select(new QCommentVO(
+                        commentEntity.userEntity.userImageUrl,
+                        commentEntity.userEntity.username,
+                        commentEntity.createdAt,
+                        commentEntity.content,
+                        commentEntity.userEntity.id,
+                        commentEntity.id,
+                        commentEntity.parentId
+                ))
+                .from(commentEntity)
+                .where(commentEntity.id.gt(cursorId));
+
+        return fetchSliceByCursor(commentEntity.getType(), commentEntity.getMetadata(), query, pageable);
+    }
+
+
+
+
 }
