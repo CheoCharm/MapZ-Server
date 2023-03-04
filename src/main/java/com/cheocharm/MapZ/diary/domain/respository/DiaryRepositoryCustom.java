78867abc180f9d636ad90d83cecb1643c8458f82
choigy1001 <easytwo10@naver.com>
2023-03-04 17:53:55 +0900

    feat: Diary 페이징, 디테일 조회 QueryDSL 메서드

--- src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustom.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustom.java
@@ -1,6 +1,8 @@
 package com.cheocharm.MapZ.diary.domain.respository;
 
 import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.cheocharm.MapZ.diary.domain.respository.vo.DiaryDetailVO;
+import com.cheocharm.MapZ.diary.domain.respository.vo.DiarySliceVO;
 import com.cheocharm.MapZ.diary.domain.respository.vo.MyDiaryVO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -8,11 +10,13 @@ import org.springframework.data.domain.Slice;
 import java.util.List;
 
 public interface DiaryRepositoryCustom {
-    List<DiaryEntity> findByUserIdAndGroupId(Long userId, Long groupId);
+    Slice<DiarySliceVO> getDiarySlice(Long userId, Long groupId, Long cursorId, Pageable pageable);
 
     Slice<MyDiaryVO> findByUserId(Long userId, Long cursorId, Pageable pageable);
 
     void deleteAllByUserId(Long userId);
 
     List<DiaryEntity> findAllByUserId(Long userId);
+
+    DiaryDetailVO getDiaryDetail(Long diaryId, Long userId);
 }
--- src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustomImpl.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryRepositoryCustomImpl.java
@@ -1,7 +1,11 @@
 package com.cheocharm.MapZ.diary.domain.respository;
 
 import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+import com.cheocharm.MapZ.diary.domain.respository.vo.DiaryDetailVO;
+import com.cheocharm.MapZ.diary.domain.respository.vo.DiarySliceVO;
 import com.cheocharm.MapZ.diary.domain.respository.vo.MyDiaryVO;
+import com.cheocharm.MapZ.diary.domain.respository.vo.QDiaryDetailVO;
+import com.cheocharm.MapZ.diary.domain.respository.vo.QDiarySliceVO;
 import com.cheocharm.MapZ.diary.domain.respository.vo.QMyDiaryVO;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -16,6 +20,8 @@ import static com.cheocharm.MapZ.comment.domain.QCommentEntity.commentEntity;
 import static com.cheocharm.MapZ.common.util.QuerydslSupport.fetchSliceByCursor;
 import static com.cheocharm.MapZ.diary.domain.QDiaryEntity.diaryEntity;
 import static com.cheocharm.MapZ.group.domain.QGroupEntity.groupEntity;
+import static com.cheocharm.MapZ.like.domain.QDiaryLikeEntity.diaryLikeEntity;
+import static com.cheocharm.MapZ.user.domain.QUserEntity.userEntity;
 
 @RequiredArgsConstructor
 public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
@@ -23,13 +29,32 @@ public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<DiaryEntity> findByUserIdAndGroupId(Long userId, Long groupId) {
-        return queryFactory
-                .selectFrom(diaryEntity)
-                .where(userIdEq(userId)
-                        .and(groupIdEq(groupId))
+    public Slice<DiarySliceVO> getDiarySlice(Long userId, Long groupId, Long cursorId, Pageable pageable) {
+        final JPAQuery<DiarySliceVO> query = queryFactory
+                .select(new QDiarySliceVO(
+                        diaryEntity.id,
+                        diaryEntity.title,
+                        diaryEntity.content,
+                        diaryEntity.address,
+                        diaryEntity.createdAt,
+                        userEntity.username,
+                        userEntity.userImageUrl,
+                        diaryEntity.diaryLikeEntities.size(),
+                        diaryLikeEntity.isNotNull(),
+                        commentEntity.count(),
+                        userEntity.id.eq(userId)
+                ))
+                .from(diaryEntity)
+                .leftJoin(diaryEntity.diaryLikeEntities, diaryLikeEntity)
+                .on(likeUserIdEq(userId))
+                .innerJoin(diaryEntity.userEntity, userEntity)
+                .leftJoin(diaryEntity.commentEntities, commentEntity)
+                .where(groupIdEq(groupId)
+                        .and(diaryIdLt(cursorId))
                 )
-                .fetch();
+                .groupBy(diaryEntity.id, diaryLikeEntity.id); //dairyLikeEntity는 nonaggregated column때문에 작성
+
+        return fetchSliceByCursor(diaryEntity.getType(), diaryEntity.getMetadata(), query, pageable);
     }
 
     @Override
@@ -46,11 +71,16 @@ public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
                 .from(diaryEntity)
                 .innerJoin(diaryEntity.groupEntity, groupEntity)
                 .leftJoin(diaryEntity.commentEntities, commentEntity)
-                .where(userIdEq(userId))
-                .where(diaryEntity.id.lt(cursorId))
+                .where(userIdEq(userId)
+                        .and(diaryIdLt(cursorId))
+                )
                 .groupBy(diaryEntity.id);
 
         return fetchSliceByCursor(diaryEntity.getType(), diaryEntity.getMetadata(), query, pageable);
+    }
+
+    private BooleanExpression diaryIdLt(Long cursorId) {
+        return diaryEntity.id.lt(cursorId);
     }
 
     @Override
@@ -69,11 +99,44 @@ public class DiaryRepositoryCustomImpl implements DiaryRepositoryCustom {
                 .fetch();
     }
 
+    @Override
+    public DiaryDetailVO getDiaryDetail(Long diaryId, Long userId) {
+        return queryFactory
+                .select(new QDiaryDetailVO(
+                        diaryEntity.title,
+                        diaryEntity.content,
+                        diaryEntity.address,
+                        diaryEntity.createdAt,
+                        userEntity.username,
+                        userEntity.userImageUrl,
+                        diaryEntity.diaryLikeEntities.size(),
+                        diaryLikeEntity.isNotNull(),
+                        commentEntity.count(),
+                        userEntity.id.eq(userId)
+                ))
+                .from(diaryEntity)
+                .leftJoin(diaryEntity.diaryLikeEntities, diaryLikeEntity)
+                .on(likeUserIdEq(userId))
+                .innerJoin(diaryEntity.userEntity, userEntity)
+                .leftJoin(diaryEntity.commentEntities, commentEntity)
+                .where(diaryIdEq(diaryId))
+                .groupBy(diaryLikeEntity.id) // nonaggregated Column 때문에 작성
+                .fetchOne();
+    }
+
+    private BooleanExpression likeUserIdEq(Long userId) {
+        return diaryLikeEntity.userEntity.id.eq(userId);
+    }
+
     private BooleanExpression userIdEq(Long userId) {
         return diaryEntity.userEntity.id.eq(userId);
     }
 
     private BooleanExpression groupIdEq(Long groupId) {
         return diaryEntity.groupEntity.id.eq(groupId);
+    }
+
+    private BooleanExpression diaryIdEq(Long diaryId) {
+        return diaryEntity.id.eq(diaryId);
     }
 }
