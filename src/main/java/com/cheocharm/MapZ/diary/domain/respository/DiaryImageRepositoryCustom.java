4041af42ca3d2c657c7c237d6c10eff11261301b
choigy1001 <easytwo10@naver.com>
2023-03-08 16:55:19 +0900

    feat: DiaryImage 삭제 Repository 메서드

--- src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryImageRepositoryCustom.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/respository/DiaryImageRepositoryCustom.java
@@ -1,9 +1,13 @@
 package com.cheocharm.MapZ.diary.domain.respository;
 
+import com.cheocharm.MapZ.diary.domain.DiaryEntity;
+
 import java.util.List;
 
 public interface DiaryImageRepositoryCustom {
     List<String> findAllByDiaryId(Long diaryId);
 
     void deleteAllByDiaryId(Long diaryId);
+
+    void deleteAllByDiaryEntityList(List<DiaryEntity> diaryEntities);
 }
