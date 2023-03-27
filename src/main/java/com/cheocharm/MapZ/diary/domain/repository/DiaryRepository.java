a252f73e5e4ce466a61c2668578ab53b775edfaf
choigy1001 <easytwo10@naver.com>
2023-03-27 13:56:38 +0900

    feat: Diary에서 좌표 획득하는 Repository 메서드

--- src/main/java/com/cheocharm/MapZ/diary/domain/repository/DiaryRepository.java
+++ src/main/java/com/cheocharm/MapZ/diary/domain/repository/DiaryRepository.java
@@ -3,7 +3,6 @@ package com.cheocharm.MapZ.diary.domain.repository;
 import com.cheocharm.MapZ.common.util.NativeQueryUtils;
 import com.cheocharm.MapZ.diary.domain.DiaryEntity;
 import com.cheocharm.MapZ.diary.domain.repository.vo.DiaryCoordinateVO;
-import com.cheocharm.MapZ.diary.domain.repository.vo.DiaryImagePreviewVO;
 import com.cheocharm.MapZ.group.domain.GroupEntity;
 import com.cheocharm.MapZ.user.domain.UserEntity;
 import org.locationtech.jts.geom.Point;
@@ -16,11 +15,6 @@ import java.util.List;
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long>, DiaryRepositoryCustom{
     List<DiaryEntity> findAllByUserEntityAndGroupEntity(UserEntity userEntity, GroupEntity groupEntity);
 
-    //VO 패키지 이동시 경로 수정 필요
     @Query(value = NativeQueryUtils.GET_COORDINATE_FROM_DIARY, nativeQuery = true)
-    List<DiaryCoordinateVO> findByDiaryCoordinate(@Param("point") Point point, @Param("groupIds") List<Long> groupIds);
-
-    @Query(value = NativeQueryUtils.GET_PREVIEW_FROM_DIARY, nativeQuery = true)
-    List<DiaryImagePreviewVO> findDiaryPreviewBy(@Param("point") String point, @Param("groupIds") List<Long> groupIds);
-
+    List<DiaryCoordinateVO> findByDiaryCoordinate(@Param("point") Point point, @Param("groupIds") List<Long> groupIds, double distance);
 }
---
+++ src/main/java/com/cheocharm/MapZ/diary/domain/repository/vo/DiaryCoordinateVO.java
@@ -0,0 +1,9 @@
+package com.cheocharm.MapZ.diary.domain.repository.vo;
+
+public interface DiaryCoordinateVO {
+    Long getDiaryId();
+
+    Double getLongitude();
+
+    Double getLatitude();
+}
