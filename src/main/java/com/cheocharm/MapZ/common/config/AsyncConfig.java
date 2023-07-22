9f95c574d4d8461573920c76e6c69e04987b0ba8
choigy1001 <easytwo10@naver.com>
2023-07-22 17:10:24 +0900

    feat: 비동기 Thread Pool 설정

---
+++ src/main/java/com/cheocharm/MapZ/common/config/AsyncConfig.java
@@ -0,0 +1,29 @@
+package com.cheocharm.MapZ.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    @Bean
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setRejectedExecutionHandler(
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+        executor.setThreadNamePrefix("async-thread-");
+        executor.initialize();
+        return executor;
+    }
+}
