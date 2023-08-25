ab2cff0dabd0c4a3550d549f2e24e81958bfd331
Keun young <easytwo10@naver.com>
2023-08-25 20:46:52 +0900

    feat: RestTemplate -> WebClient 변경 (#200)

--- src/main/java/com/cheocharm/MapZ/common/oauth/OauthApi.java
+++ src/main/java/com/cheocharm/MapZ/common/oauth/OauthApi.java
@@ -9,6 +9,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Deprecated
 @RequiredArgsConstructor
 @Component
 public class OauthApi {
